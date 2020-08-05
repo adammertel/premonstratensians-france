78d556e1cfc889f5f6b45b5ebadbd2a2870cf1d4
adammmertel <mertel.adam@gmail.com>
2020-08-05 15:27:41 +0200

    Create hero component

---
+++ map-interactive/app/components/hero.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+import { globals } from "./../index";
+
+type HeroProps = {};
+
+export const Hero: React.FC<HeroProps> = ({}) => {
+  return (
+    <div
+      className="hero font-bold text-xl pt-12 pb-4 px-4 text-white bg-cover"
+      data-testid="welcome-wrapper"
+      style={{
+        background: " cover",
+        backgroundImage: `url(
+            "https://cdn.muni.cz/media/3132847/out_gradient.png?mode=crop&center=0.54,0.86&rnd=131981761180000000&width=974&heightratio=0.23715"
+          )`,
+      }}
+    >
+      <h2 className="heading text-white font-bold">
+        Premonstratensian monasteries
+      </h2>
+      <h4 className="subheading text-white font-medium">
+        Premonstratensian monasteries
+        <br />
+        iin France (from Ardura 1993) (v. {globals.version})
+      </h4>
+    </div>
+  );
+};
