73f2489ade9bb5f769492327c4b9fbdd6d7f3bff
adammmertel <mertel.adam@gmail.com>
2020-08-05 15:27:51 +0200

    Add globals variables

--- map-interactive/app/index.tsx
+++ map-interactive/app/index.tsx
@@ -1,13 +1,19 @@
 import "./app.css";
 import "./../node_modules/leaflet/dist/leaflet.css";
 
-import React, { Fragment } from "react";
+import React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/app";
 import Store from "./store";
 
-window["store"] = new Store();
+var globals = {
+  store: new Store(),
+  version: process.env.npm_package_version,
+};
+
 ReactDOM.render(
-  React.createElement(App, { store: window["store"] }),
+  React.createElement(App, { store: globals["store"] }),
   document.getElementById("app")
 );
+
+export { globals };
