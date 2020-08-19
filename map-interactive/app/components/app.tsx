e3b9f8a4743ffe2f22fbf98fe719284244dfe0f6
adammmertel <mertel.adam@gmail.com>
2020-08-19 11:06:43 +0200

    Create button for hiding panel

--- map-interactive/app/components/app.tsx
+++ map-interactive/app/components/app.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { MapComponent } from "./map";
 import { Panel } from "./panel";
 import { Welcome } from "./welcome";
@@ -9,16 +9,38 @@ type Props = {
 };
 
 export const App: React.FC<Props> = observer(({ store }) => {
+  const [panelDisplay, setPanelDisplay] = useState(true);
   return (
-    <div>
+    <div className={panelDisplay ? "panel-on" : "panel-off"}>
       {store.openWelcome && <Welcome store={store} />}
+      {panelDisplay ? (
+        <button
+          className="m-0 panel-hider on text-base muni bg-black"
+          onClick={() => {
+            setPanelDisplay(false);
+          }}
+          title="toggle panel"
+        >
+          <i className="text-bold icon icon-arrow-alt-from-left" />
+        </button>
+      ) : (
+        <button
+          className=" m-0 panel-hider off text-base muni bg-black"
+          onClick={() => {
+            setPanelDisplay(true);
+          }}
+          title="toggle panel"
+        >
+          <i className="text-bold icon icon-arrow-alt-from-right" />
+        </button>
+      )}
       <MapComponent
         handleMapMoved={store.mapMoved.bind(store)}
         center={store.center}
         zoom={store.zoom}
         data={store.activeData}
       />
-      <Panel store={store} />
+      {panelDisplay && <Panel store={store} />}
     </div>
   );
 });
